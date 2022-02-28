6a8801ec2b85afda68402ff8cfe03a7a24de27f2
adil93 <aubakir.adilbek@gmail.com>
2022-02-28 23:36:01 +0600

    add current month method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,4 +41,9 @@ class HomeController extends Controller
     {
         return Carbon::now()->year;
     }
+
+    public function getCurrentMonth()
+    {
+        return Carbon::now()->year;
+    }
 }
